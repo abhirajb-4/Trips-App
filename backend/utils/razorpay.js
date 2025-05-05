46107a0ce9ebcc373e7bbbc36d151cbd8bc953ae
ABHIRAJ B <144231717+abhirajb-4@users.noreply.github.com>
2025-05-05 10:26:00 +0530

    Update razorpay.js

--- backend/utils/razorpay.js
+++ backend/utils/razorpay.js
@@ -1,8 +1,8 @@
 const Razorpay = require('razorpay');
 
 const razorpay = new Razorpay({
-  key_id: 'rzp_test_Va99OvjJU36gup',         // Replace with your Test Key ID
-  key_secret: '4Kvrx9qFr1qHLBgTRt6s3whk'         // Replace with your Test Secret
+  key_id: 'rzp_test_69kRzvitkm0bu3',         
+  key_secret: 'Eo1O1PiP7zDCQUtwMhByZv6w'         
 });
 
 module.exports = razorpay;
